0b9257c8707170fa0dd30ddfba5aedb8bf6e833e
terumafu <119095669+terumafu@users.noreply.github.com>
2024-10-14 12:02:08 -0700

    step 1 added button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "My amazing cookies";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const button = document.createElement("button");
+button.innerHTML = "button";
 app.append(header);
+app.append(button);
+button.addEventListener('click', ()=>{
+    console.log("button clicked");
+})
