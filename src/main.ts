a5d1948d27c6bc46ac82f4578cfb7e57aa42107e
terumafu <119095669+terumafu@users.noreply.github.com>
2024-10-14 15:16:49 -0700

    step 3 complete: incremtal growth

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,14 @@ div.innerHTML = laughCount + " Laughs";
 const button = document.createElement("button");
 button.innerHTML = "😆";
 
+setInterval(incrementNum,1000)
+//calls incrementNum every second
 app.append(header);
 app.append(button);
-app.append(div)
-button.addEventListener("click", () => {
-  console.log("button clicked");
+app.append(div);
+button.addEventListener("click", incrementNum);
+
+function incrementNum(){
   laughCount++;
   div.innerHTML = laughCount + " Laughs";
-});
-
-
+}
