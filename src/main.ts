db9fc6937bd8a2af0327d2df836a667c4ea437be
terumafu <119095669+terumafu@users.noreply.github.com>
2024-10-14 15:12:30 -0700

    step 2 complete: added counter

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,27 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+let laughCount: number = 0;
+
 const gameName = "My amazing cookies";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 
+const div = document.createElement("div");
+div.innerHTML = laughCount + " Laughs";
+
 const button = document.createElement("button");
 button.innerHTML = "😆";
+
 app.append(header);
 app.append(button);
+app.append(div)
 button.addEventListener("click", () => {
   console.log("button clicked");
+  laughCount++;
+  div.innerHTML = laughCount + " Laughs";
 });
+
+
