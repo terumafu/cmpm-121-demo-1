afc4f9d37d959b0036c05588fb70009823daf227
terumafu <119095669+terumafu@users.noreply.github.com>
2024-10-14 17:48:02 -0700

    step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -4,62 +4,87 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 let laughCount: number = 0;
 let prevTime: number = 0;
-let increments : number = 0;
-const gameName = "My amazing cookies";
+let increments: number = 0;
+
+interface Item{
+  name :string,
+  cost : number,
+  button : HTMLButtonElement,
+  output : number,
+  count : number,
+  counttext : HTMLDivElement,
+}
+
+function buyItem(item : Item){
+  if (laughCount >= item.cost){
+    increments += item.output;
+    laughCount -= item.cost;
+    item.count ++;
+    item.counttext.innerHTML = item.count + " " + item.name;
+  }
+}
+function buttonInit(item : Item){
+  item.button.innerHTML = item.name + " Costs " + item.cost;
+  item.button.disabled = true;
+  item.button.addEventListener("click", () => buyItem(item));
+  
+  item.counttext.innerHTML = item.count + " " + item.name;
+
+  app.append(item.button);
+  app.append(item.counttext);
+}
+const gameName = "Laugh Generator";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 
-const div = document.createElement("div");
-div.innerHTML = laughCount + " Laughs";
+const count = document.createElement("div");
+count.innerHTML = laughCount + " Laughs";
+
+const incrementRate = document.createElement("div");
+incrementRate.innerHTML = increments + "/sec";
 
 const firstbutton = document.createElement("button");
 firstbutton.innerHTML = "😆";
 
 
-const newspaperFunnies = document.createElement("button");
-let newspaperFunniesCost = 10;
-let newspaperFunniesOut = 1;
-newspaperFunnies.innerHTML = "Newspaper Funnies Costs " + newspaperFunniesCost;
-newspaperFunnies.disabled = true;
-
-//setInterval(incrementNum, 1000);
-//calls incrementNum every second
 app.append(header);
 app.append(firstbutton);
-app.append(div);
-app.append(newspaperFunnies)
-
+app.append(count);
+app.append(incrementRate);
 firstbutton.addEventListener("click", incrementNum);
-newspaperFunnies.addEventListener("click", ()=>{
-  if (laughCount >= newspaperFunniesCost){
-    increments += newspaperFunniesOut;
-    laughCount -= newspaperFunniesCost;
-  }
- 
 
-})
+
+
+const newspaperFunnies : Item = {name: "Newspaper Funnies",cost:10, button: document.createElement("button"),output: 1, count: 0, counttext: document.createElement("div")};
+buttonInit(newspaperFunnies)
+
+const streetMime : Item = {name: "Street Mimes", cost: 100,button: document.createElement("button"), output: 2, count: 0,counttext: document.createElement("div")};
+buttonInit(streetMime);
 
 function incrementNum() {
   laughCount++;
-  div.innerHTML = laughCount + " Laughs";
+  count.innerHTML = laughCount + " Laughs";
 }
+
 function animate(): void {
   //every frame calls this
-  let timepassed = (performance.now() - prevTime) / 1000.0
-  
+  const timepassed = (performance.now() - prevTime) / 1000.0;
+
   laughCount += increments * timepassed;
   prevTime = performance.now();
-  div.innerHTML = laughCount.toFixed(1) + " Laughs";
+  count.innerHTML = laughCount.toFixed(1) + " Laughs";
+  incrementRate.innerHTML = increments + "/sec";
   requestAnimationFrame(animate);
-  checkdisabled(newspaperFunnies,10);
+  checkdisabled(newspaperFunnies);
+  checkdisabled(streetMime)
 }
 
 requestAnimationFrame(animate);
 
-function checkdisabled(El : HTMLButtonElement, threshold : number){
-  if (El.disabled == true && laughCount >= threshold){
-    El.disabled = false;
+function checkdisabled(item: Item) {
+  if (item.button.disabled == true && laughCount >= item.cost) {
+    item.button.disabled = false;
   }
 }
